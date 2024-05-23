ab1a5d0760fbcedb20f9588d1f741bdd16624ca3
Yoshua Kock <488350@student.fontys.nl>
2024-05-24 01:13:14 +0200

    dropdown menu for stories as depicted on figma and discover page with interactive map which can have locations marked in js file

---
+++ js/map.js
@@ -0,0 +1,14 @@
+var map = L.map('map').setView([51.590, 4.7800], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// To add circle on specific locations in Breda
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
